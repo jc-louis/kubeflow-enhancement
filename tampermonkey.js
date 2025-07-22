b5ec75e0ff3a5882d56c52ca2202628774f6d39a
JC Louis <jean-charles.louis@lifen.fr>
2025-07-22 13:53:35 +0200

    tampermonkey.js

--- tampermonkey.js
+++ tampermonkey.js
@@ -4,7 +4,6 @@
 // @version      1.3
 // @description  Enhance Kubeflow
 // @author       jc-louis
-// @source       https://github.com/jc-louis/kubeflow-enhancement
 // @match        https://kubeflow-v2.private.lifen.fr/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kubeflow.org
 // @grant        none
@@ -18,33 +17,20 @@
 
     console.log("Kubeflow Enhancement script v1.3 loaded");
 
-    function getCookie(name) {
-        const value = `; ${document.cookie}`;
-        const parts = value.split(`; ${name}=`);
-        if (parts.length === 2) {
-            return parts.pop().split(';').shift();
-        }
-        return null;
-    }
-
     function findAndReplaceInput() {
         const inputs = document.querySelectorAll('input[type="text"], input[type="email"], input:not([type])');
-
+    
         for (let input of inputs) {
             if (input.value === "unknown@lifen.fr") {
                 console.log("Found input with unknown@lifen.fr:");
-
-
+    
                 const userEmail = getCookie('ajs_user_id');
-
+    
                 if (userEmail) {
                     console.log("Replacing with email from cookie:", userEmail);
-                    input.value = userEmail;
-
-                    // Trigger input events to ensure the change is registered
-                    input.dispatchEvent(new Event('input', { bubbles: true }));
-                    input.dispatchEvent(new Event('change', { bubbles: true }));
-
+    
+                    setNativeValue(input, userEmail);
+    
                     return true;
                 } else {
                     console.warn("Could not find ajs_user_id cookie");
@@ -52,6 +38,13 @@
             }
         }
         return false;
+    }
+    
+    function setNativeValue(element, value) {
+        const valueSetter = Object.getOwnPropertyDescriptor(element.__proto__, 'value').set;
+        valueSetter.call(element, value);
+        element.dispatchEvent(new Event('input', { bubbles: true }));
+        element.dispatchEvent(new Event('change', { bubbles: true }));
     }
 
     function startObserving() {
